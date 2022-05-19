dd9536c41c403fe02b6d488248b2b85a4ce394ed
Juan Serna <juandavidserna12@gmail.com>
2022-05-18 23:16:48 -0500

    Una primera soluciones al reto 4

---
+++ Ciclo1/reto4-1.py
@@ -0,0 +1,22 @@
+import json
+
+pedidos = input()
+edificio = input()
+
+edificio = edificio.upper()
+edificio = list(edificio)
+
+pedidos = json.loads(pedidos)
+
+cantidad = 0
+ediFound = []
+
+for key in edificio:
+    if key in pedidos:
+        cantidad += pedidos[key]
+        ediFound.append(key)
+
+print(cantidad)
+print(' '.join(ediFound))
+
+
